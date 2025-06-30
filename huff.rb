b9b89eeaabcfa06ac483b1ba31eb66b602cbf43a
AyushChakraborty <chakrabortyayush83@gmail.com>
2025-06-30 13:20:09 +0530

    add updated shasum for v0.1.0

--- huff.rb
+++ huff.rb
@@ -2,7 +2,7 @@ class Huff < Formula
   desc "A CLI tool for Huffman algorithm based file compression and decompression"
   homepage "https://github.com/AyushChakraborty/huffzip"
   url "https://github.com/AyushChakraborty/huffzip/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "abcd3f58cda9daed8d35d3aff531a07d1f8e03f174d3fbcb93cd64801b5986fa"
+  sha256 "2340b30a35b2b23c79cad2ddf4124809ad59cd153c75a57d6aebae29f338df89"
   license "MIT"
 
   def install 
